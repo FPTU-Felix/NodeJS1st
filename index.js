82b01a58fd10fb59ecbc1e0ac6ed76284d10c729
Felix <sontug9xx@gmail.com>
2025-05-11 10:40:04 +0700

    Push Source

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const app  = express();
+const port = 3000;
+//test
+
+app.use(morgan('combined'));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+//127.0.0.1 - localhost
+app.get('/about', (req, res) => {
+  res.send('Testing git repository');
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
